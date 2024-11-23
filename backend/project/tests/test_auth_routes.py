4eb40ed2e3c165940a8d9d571238c8a5b6ebd4ab
unknown <everaldovcjr@gmail.com>
2024-11-23 13:27:51 -0300

    fourth test pass

--- backend/project/tests/test_auth_routes.py
+++ backend/project/tests/test_auth_routes.py
@@ -17,7 +17,8 @@ def valid_login_data():
 @pytest.fixture
 def invalid_login_data():
     return  {
-        "email": "valid@email.com"
+        "email": "valid@email.com",
+        "password": ""
     }
 
 
