7ac8eea791cff4b3c0f169d6439fdcca95de10d5
unknown <everaldovcjr@gmail.com>
2024-11-23 14:21:33 -0300

    User.create_user() failed. The function create a user with blank email

--- backend/project/tests/test_auth_routes.py
+++ backend/project/tests/test_auth_routes.py
@@ -19,18 +19,19 @@ def invalid_login_data():
 
 @pytest.fixture
 def login_form(valid_login_data):
-    return LoginForm(valid_login_data).is_valid()
+    form = LoginForm(valid_login_data)
+    return form.is_valid()
 
 
 @pytest.fixture
-def auth(valid_login_data):
-    user = authenticate(None, **valid_login_data)
+def auth(invalid_login_data):
+    user = authenticate(None, **invalid_login_data)
     return user, user is not None
 
 
 @pytest.fixture
-def create_user(django_user_model, valid_login_data):
-    user = django_user_model.objects.create_user(**valid_login_data)
+def create_user(django_user_model, invalid_login_data):
+    user = django_user_model.objects.create_user(**invalid_login_data)
     return user
 
 
@@ -38,9 +39,9 @@ def create_user(django_user_model, valid_login_data):
 @pytest.mark.django_db
 def test_login_form(create_user, login_form, auth):
 
-    assert login_form == True
+    assert login_form
     user, isNoNone = auth
-    assert isNoNone == True
+    assert isNoNone
     assert user == create_user
 
 
