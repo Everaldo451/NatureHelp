ec35a858d338fa8b6e7b151c0aad6911aebb3115
unknown <everaldovcjr@gmail.com>
2024-11-23 14:01:11 -0300

    create user function

--- backend/project/tests/test_auth_routes.py
+++ backend/project/tests/test_auth_routes.py
@@ -27,8 +27,15 @@ def auth(valid_login_data):
     return authenticate(None, **valid_login_data) is not None
 
 
+@pytest.fixture
+def create_user(django_user_model, valid_login_data):
+    user = django_user_model.objects.create_user(**valid_login_data)
+    return user
+
+
+
 @pytest.mark.django_db
-def test_login_form(login_form, auth):
+def test_login_form(create_user, login_form, auth):
 
     assert login_form == True
     assert auth == True
