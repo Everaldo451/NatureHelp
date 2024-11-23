2063e6abe8a163b70dc215e95af68b479e51fdea
unknown <everaldovcjr@gmail.com>
2024-11-23 14:46:26 -0300

    new test

--- backend/project/tests/test_auth_routes.py
+++ backend/project/tests/test_auth_routes.py
@@ -1,6 +1,8 @@
 import pytest
 from authe.form import LoginForm
 from django.contrib.auth import authenticate
+from django.http import HttpResponse
+from rest_framework.response import Response
 
 @pytest.fixture
 def valid_login_data():
@@ -17,6 +19,7 @@ def invalid_login_data():
         "password": ""
     }
 
+
 @pytest.fixture
 def login_form(valid_login_data):
     form = LoginForm(valid_login_data)
@@ -24,8 +27,8 @@ def login_form(valid_login_data):
 
 
 @pytest.fixture
-def auth(invalid_login_data):
-    user = authenticate(None, **invalid_login_data)
+def auth(valid_login_data):
+    user = authenticate(None, **valid_login_data)
     return user, user is not None
 
 
@@ -35,14 +38,17 @@ def create_user(django_user_model, valid_login_data):
     return user
 
 
-
 @pytest.mark.django_db
-def test_login_form(create_user, login_form, auth):
+def test_login_route(create_user, login_form, auth, response):
 
     assert login_form
     user, isNoNone = auth
     assert isNoNone
     assert user == create_user
+    assert isinstance(response, Response)
+    assert response.cookies.get("access_token")
+    assert response.cookies.get("refresh_token")
+    assert type(response.content) == dict[str, str]
 
 
 
