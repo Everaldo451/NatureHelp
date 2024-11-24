5e9bce38ec340eddcdb862cf0e257c0b4def0cd6
unknown <everaldovcjr@gmail.com>
2024-11-23 21:58:38 -0300

    test passed

--- backend/project/tests/test_register_route.py
+++ backend/project/tests/test_register_route.py
@@ -1,5 +1,10 @@
 from authe.form import RegisterFormForCompany, RegisterFormForUser
+from api.models import Company
 import pytest
+
+@pytest.fixture
+def company_model():
+    return Company
 
 @pytest.fixture
 def user_data():
@@ -17,20 +22,27 @@ def company_form(user_data):
 
 
 @pytest.fixture
-def create_company_user(user_data):
-    pass
+def create_company_user(django_user_model,user_data):
+    user = django_user_model.objects.create_user(
+        email=user_data.get("email"),
+        password=user_data.get("password")
+    )
+    return user
 
 @pytest.fixture
-def create_company(user_data):
-    pass
+def create_company(company_model, user_data):
+    company = company_model(
+        name = user_data.get("name"),
+        CNPJ = user_data.get("CNPJ"),
+
+    )
+    return company
 
 
 @pytest.mark.django_db
 def test_with_user_is_company(company_form, create_company_user,  create_company):
 
-    user = create_company_user
-    company = create_company
 
     assert company_form
-    assert user is not None
-    assert company is not None
+    assert create_company_user is not None
+    assert create_company is not None
