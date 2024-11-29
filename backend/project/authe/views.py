60ced69bff4ff21f264bf547385cf84b9109c350
unknown <everaldovcjr@gmail.com>
2024-11-29 12:01:32 -0300

    modifyng with pylint

--- backend/project/authe/views.py
+++ backend/project/authe/views.py
@@ -1,20 +1,20 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.exceptions import TokenError
+from django.http import HttpRequest
 from django.contrib.auth import authenticate
-from django.contrib import messages
 from django.shortcuts import redirect
-from django.db import IntegrityError, transaction
+from django.db import DatabaseError, transaction
 from django.views.decorators.csrf import csrf_protect
 from api.models import Company
 from .models import User
 from .form import LoginForm, RegisterFormForCompany, RegisterFormForUser
 from .utils import generate_tokens, generate_token_response
 from .serializers import UserSerializer
-from django.http import HttpRequest
 
 
 @api_view(["GET"])
@@ -22,32 +22,30 @@ def get_jwt(request):
 
 	if request.COOKIES.get("refresh_token"):
 		try:
-			
-			refresh = RefreshToken(request.COOKIES.get("refresh_token"))
+			refresh = RefreshToken(request.COOKIES.get("refresh_token"), True)
 			return generate_token_response(refresh)
-		
-		except: return Response(None)
+		except TokenError as err: 
+			print(err.args)
+			return Response(None)
 	return Response(None)
 
 
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def get_user(request):
-
-	try:
-		serializer = UserSerializer(request.user)
-		serializer = serializer.data
-		serializer.pop("id")
-		if not request.user.groups.filter(name="Company").exists():
-			print("notCompany")
-			serializer.pop("company")
-
-		return Response(serializer)
-
-	except Exception as e: 
-		return Response(None)
 	
+	serializer = UserSerializer(request.user)
+	if not serializer.is_valid():
+		return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+	data = serializer.data
+	data.pop("id")
+	if not request.user.groups.filter(name="Company").exists():
+		print("notCompany")
+		data.pop("company")
 
+	return Response(data)
+
+	
 @api_view(["POST"])
 @csrf_protect
 def login(request):
@@ -62,29 +60,27 @@ def login(request):
 			password = form.cleaned_data.get("password")
 		)
 
-		if user is not None and user.is_active:
+		if user is None or not user.is_active:
 
-			return generate_tokens(request, user)
-			
-		else:
 			print("No user")
-			messages.error(request,"No user")
-			return Response(None, status=status.HTTP_401_UNAUTHORIZED)
+			return Response("No user", status=status.HTTP_401_UNAUTHORIZED)
+
+		return generate_tokens(request, user)
+		
 	
-	else: return Response(None, status=status.HTTP_401_UNAUTHORIZED)
-	
+	return Response(None, status=status.HTTP_401_UNAUTHORIZED)	
 
 @api_view(["POST"])
 @csrf_protect
 def register(request):
 
-	formForUser = RegisterFormForUser(request.POST)
-	formForCompany = RegisterFormForCompany(request.POST)
+	user_form = RegisterFormForUser(request.POST)
+	company_form = RegisterFormForCompany(request.POST)
 
 
-	if formForCompany.is_valid():
+	if company_form.is_valid():
 
-		user_data = formForCompany.cleaned_data
+		user_data = company_form.cleaned_data
 
 		try:
 
@@ -100,14 +96,12 @@ def register(request):
 
 			return generate_tokens(request, user)
 				
-		except:
-
+		except DatabaseError:
 			return Response(None, status=status.HTTP_400_BAD_REQUEST)
 	
-	elif formForUser.is_valid():
+	elif user_form.is_valid():
 
-		user_data = formForUser.cleaned_data
-
+		user_data = user_form.cleaned_data
 		try:
 
 			full_name = user_data.get("full_name")
@@ -118,17 +112,19 @@ def register(request):
 
 			user = User.objects.create_user(
 
-				email=formForCompany.cleaned_data.get("email"),
-				password = formForCompany.cleaned_data.get("password"),
+				email=user_data.get("email"),
+				password = user_data.get("password"),
 				first_name = first_name,
 				last_name = last_name
 			)
 
 			return generate_tokens(request, user)
 		
-		except IndexError as e:
+		except IndexError as err:
+			print(err.args)
 			return Response({"Insira um nome completo"}, status=status.HTTP_400_BAD_REQUEST)
-		except IntegrityError as e:
+		except DatabaseError as err:
+			print(err.args)
 			return Response(None, status=status.HTTP_400_BAD_REQUEST)
 	else: 
 		print("not valid")
@@ -142,7 +138,3 @@ def logout(request):
 	response.delete_cookie("refresh_token")
 
 	return response
-	
-	
-
-# Create your views here.
