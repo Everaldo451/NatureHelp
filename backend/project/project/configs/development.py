9c68f9887390ac56da2ba08c3ab1432be4179498
unknown <everaldovcjr@gmail.com>
2024-12-01 13:07:40 -0300

    MySQL use

--- backend/project/project/configs/development.py
+++ backend/project/project/configs/development.py
@@ -3,10 +3,3 @@ from .base import *
 DEBUG = True
 
 SECRET_KEY = secrets.token_urlsafe(50)
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'test.sqlite3',
-    }
-}
--- backend/project/project/configs/production.py
+++ backend/project/project/configs/production.py
@@ -5,9 +5,3 @@ DEBUG = False
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
